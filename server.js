9b74238d64c38d7050db2b0af2b488406c7d8ff2
Chronomly <chronomly@gmail.com>
2018-10-02 21:22:38 -0400

    fix it all my dudes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveIndex = require('serve-index');
 const webconfig = require("./webconfig")
 
 app.use(express.static("public_html"))
-app.use('/data', serveIndex(__dirname + '/data'));
+// app.use('/data', serveIndex(__dirname + '/data'));
 app.get('/discord', function(req, res){
    res.redirect(301, `https://discord.gg/SFytdyB`)
    console.log(`Discord Redirect sent to ${req.ip}`)
@@ -15,6 +15,8 @@ app.get('/*', function(req, res){
    res.sendFile(__dirname + '/public_html/error.html');
    console.log(`Error Page sent to ${req.ip}`)
 });
+
+
  
 app.listen(9000, running)
 function running() {
